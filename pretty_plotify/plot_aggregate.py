472c068a292ac747d7c6afbed7504b71cbd513f9
Florentin Rochet <florentin.rochet@gmail.com>
2021-01-15 15:59:00 +0100

    add plot scripts

--- pretty_plotify/plot_aggregate.py
+++ pretty_plotify/plot_aggregate.py
@@ -14,8 +14,10 @@ parser.add_argument("--tcpdump", type=str, help="tcpdump trace")
 parser.add_argument("--oname", type=str, help="output figure  name")
 
 parser.add_argument("-i", type=float, default=0.1, help="Time interval on which each bandwidth datapoint is computed")
-parser.add_argument("--newpath_at", type=str, help="Breakage happened at? in\
+parser.add_argument("--event_at", type=str, nargs="+", help="Event happened at? in\
                     second")
+parser.add_argument("--event_text", type=str, nargs="+", help="Event text linked to the event_at")
+parser.add_argument("--event_pos", type=int, nargs="+", help="Event text position on the y axis")
 
 def parse_time(timestr):
     """
@@ -141,14 +143,18 @@ if __name__ == "__main__":
     for path in set(paths.values()):
         ax.plot([(x-min_timing_t)/1000000 for x in x_t[path][:-1]],
                 [(y*8/1000000)/(2*args.i) for y in y_t[path][:-1]],
-                label="Throughput Path {}".format(counter))
+                label="Throughput TCP Conn {}".format(counter))
         counter+=1
     min_timing = min(min_timing, min_timing_t)
-    newpath = parse_time(args.newpath_at)
-    plt.axvline(x=(newpath-min_timing)/1000000, color="k")
-    plt.text((newpath-min_timing)/1000000, 3, "<- Client is attaching a new stream",
-             color="k")
+    counter = 0
+    for event in args.event_at:
+        event = parse_time(event)
+        plt.axvline(x=(event-min_timing)/1000000, color="k")
+        plt.text((event-min_timing)/1000000, args.event_pos[counter], args.event_text[counter],
+                 color="k")
+        counter+=1
     plt.legend()
-    plt.savefig(args.oname)
+    #plt.savefig(args.oname)
+    plt.show()
 
 
---
+++ pretty_plotify/plot_migration.sh
@@ -0,0 +1,2 @@
+python plot_aggregate.py --goodput results/tcpls_migration_goodput --tcpdump results/tcpls_migration_pruned.log --oname migration -i 0.1 --event_at 14:55:28.854711 14:55:30.142117 14:55:33.569810 14:55:34.752775 --event_text "<- New Stream on Path 1" "<- Stream closed and Path 0 Closed" "<- 0-RTT Conn + New Stream on Path 0" "<- Stream Closed and Path 1 closed" --event_pos 2 7 2 7
+
